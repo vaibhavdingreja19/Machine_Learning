cc7b6afdb50773baef20ff116039e6e8113fbd72
vaibhavdingreja19 <37464931+vaibhavdingreja19@users.noreply.github.com>
2019-09-02 02:32:22 +0530

    Add files via upload

---
+++ linear_regression_from_scratch.py
@@ -0,0 +1,35 @@
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt
+
+xs=np.array([1,2,3,4,5,6], dtype=np.float64)
+ys=np.array([5,4,6,5,6,7], dtype=np.float64)
+
+def best_fit_slope_and_intercept(xs,ys):
+    m = ( ((mean ( xs ) * mean (ys)) - mean(xs*ys ) ) / 
+         ((mean (xs)* mean(xs)) - mean(xs*xs) ))
+    b = mean(ys)-m * mean(xs)
+    return m,b
+
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line-ys_orig)**2)
+
+def coeff_of_deter(ys_orig,ys_line):
+    ys_mean_line=[mean(ys_orig) for y in ys_orig]
+    squared_error_regg=squared_error(ys_orig,ys_line)
+    squared_error_y_mean=squared_error(ys_orig,ys_mean_line)
+    return(1-(squared_error_regg/squared_error_y_mean))
+
+m,b=best_fit_slope_and_intercept(xs,ys)
+regression_line = [(m*x)+b for x in xs]
+
+predict_x=9
+predict_y=(m*predict_x)+b
+
+r_squared=coeff_of_deter(ys,regression_line)
+print(r_squared)
+
+plt.scatter(xs,ys)
+plt.scatter(predict_x,predict_y)
+plt.plot(xs,regression_line)
+
